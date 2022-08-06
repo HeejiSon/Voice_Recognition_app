46093454c4685ecfc03757c8c860d31a3d700053
HeejiSon <thsgmlwl@gmail.com>
2022-08-06 23:15:11 +0900

    시각장애인을 위한 음성인식 앱

---
+++ app/src/main/java/com/example/voicerecognition_app/Login.java
@@ -0,0 +1,36 @@
+package com.example.voicerecognition_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+    TextView sign;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //로그인 버튼
+        login = findViewById(R.id.loginbutton);
+
+        login.setOnClickListener( v -> {
+            Intent intent1 = new Intent(this, menu_tab.class);
+            startActivity(intent1);
+        });
+
+        //회원가입 버튼
+        sign = findViewById(R.id.signin);
+
+        //회원가입 버튼 클릭시, 회원가입 페이지로 이동
+        sign.setOnClickListener(v -> {
+            Intent intent2 = new Intent(this, Signup.class);
+            startActivity(intent2);
+        });
+    }
+}
---
+++ app/src/main/java/com/example/voicerecognition_app/Signup.java
@@ -0,0 +1,14 @@
+package com.example.voicerecognition_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
