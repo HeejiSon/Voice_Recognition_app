31d8994e2d49d29010896a89758c97fdac9d6427
HeejiSon <thsgmlwl@gmail.com>
2022-08-24 16:09:36 +0900

    메뉴 선택완료 화면

---
+++ app/src/main/java/com/example/voicerecognition_app/order_check.java
@@ -0,0 +1,14 @@
+package com.example.voicerecognition_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class order_check extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_check);
+    }
+}
